6ca6b5e4d4d42179c7538ddfbedbb40ecdff58ae
Kucharssim <kucharssim@gmail.com>
2020-09-14 12:11:45 +0200

    update tests

--- tests/testthat/test-ldchisq.R
+++ tests/testthat/test-ldchisq.R
@@ -1,30 +1,26 @@
 context("Discover Distributions - Chi-square")
 
 options <- jaspTools::analysisOptions("LDchisq")
-options$.meta <- list(newVariableName = list(containsColumn = TRUE), variable = list(
-  containsColumn = TRUE))
-options$andersonDarling <- TRUE
-options$ciInterval <- TRUE
-options$cramerVonMisses <- TRUE
-options$ecdf <- TRUE
-options$estCDF <- TRUE
-options$estPDF <- TRUE
 options$explanatoryText <- TRUE
-options$highlightDensity <- TRUE
-options$highlightProbability <- TRUE
-options$histogram <- TRUE
-options$kolmogorovSmirnov <- TRUE
-options$methodMLE <- TRUE
-options$moments <- TRUE
-options$newVariableName <- ""
-options$outputSE <- TRUE
 options$parsSupportMoments <- TRUE
 options$plotCDF <- TRUE
 options$plotQF <- TRUE
-options$ppplot <- TRUE
+options$methodMLE <- TRUE
+options$outputSE <- TRUE
+options$ciInterval <- TRUE
+options$estPDF <- TRUE
 options$qqplot <- TRUE
-options$summary <- TRUE
+options$estCDF <- TRUE
+options$ppplot <- TRUE
+options$kolmogorovSmirnov <- TRUE
+options$cramerVonMisses <- TRUE
+options$andersonDarling <- TRUE
+options$highlightDensity <- TRUE
+options$highlightProbability <- TRUE
 options$variable <- "Chisq1000(df=5,ncp=0)"
+options$moments <- TRUE
+options$ecdf <- TRUE
+options$newVariableName <- ""
 set.seed(1)
 results <- jaspTools::runAnalysis("LDchisq", "Distributions.csv", options)
 
@@ -32,25 +28,25 @@ results <- jaspTools::runAnalysis("LDchisq", "Distributions.csv", options)
 test_that("Empirical Cumulative Distribution plot matches", {
   plotName <- results[["results"]][["dataContainer"]][["collection"]][["dataContainer_ecdf"]][["data"]]
   testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-  jaspTools::expect_equal_plots(testPlot, "empirical-cumulative-distribution", dir="LDchisq")
+  expect_equal_plots(testPlot, "empirical-cumulative-distribution", dir="LDchisq")
 })
 
 test_that("Histogram plot matches", {
   plotName <- results[["results"]][["dataContainer"]][["collection"]][["dataContainer_histogram"]][["data"]]
   testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-  jaspTools::expect_equal_plots(testPlot, "histogram", dir="LDchisq")
+  expect_equal_plots(testPlot, "histogram", dir="LDchisq")
 })
 
 test_that("Observed Moments table results match", {
   table <- results[["results"]][["dataContainer"]][["collection"]][["dataContainer_moments"]][["data"]]
-  jaspTools::expect_equal_tables(table,
+  expect_equal_tables(table,
                       list(5.06873210797175, 1, 5.06873210797175, 10.4688174303449, 2, 36.1608626127286
                       ))
 })
 
 test_that("Descriptives table results match", {
   table <- results[["results"]][["dataContainer"]][["collection"]][["dataContainer_summary"]][["data"]]
-  jaspTools::expect_equal_tables(table,
+  expect_equal_tables(table,
                       list(21.9144146199972, 5.06873210797175, 4.38244055093348, 0.175664757015611,
                            2.68285589535141, 6.70891916895853, 1000, 3.2371741885589, 10.479296727072,
                            "Chisq1000(df=5,ncp=0)"))
@@ -58,58 +54,58 @@ test_that("Descriptives table results match", {
 
 test_that("Estimated Parameters table results match", {
   table <- results[["results"]][["mleContainer"]][["collection"]][["mleContainer_estParametersTable"]][["data"]]
-  jaspTools::expect_equal_tables(table,
-                      list(4.89430766995065, 4.47318516594046, "k", 0.214862368559807, 5.31543017396084,
-                           0.174886673572473, -0.295176292246842, "<unicode>", 0.239832450762928,
-                           0.644949639391788))
+  expect_equal_tables(table,
+                      list(4.8938297545344, 4.47271658799517, "k", 0.214857604456471, 5.31494292107364,
+                           0.174944391692288, -0.295081957134747, "<unicode>", 0.239813768280715,
+                           0.644970740519323))
 })
 
 test_that("Empirical vs. Theoretical CDF plot matches", {
   plotName <- results[["results"]][["mleContainer"]][["collection"]][["mleContainer_mleFitAssessment"]][["collection"]][["mleContainer_mleFitAssessment_estCDF"]][["data"]]
   testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-  jaspTools::expect_equal_plots(testPlot, "empirical-vs-theoretical-cdf", dir="LDchisq")
+  expect_equal_plots(testPlot, "empirical-vs-theoretical-cdf", dir="LDchisq")
 })
 
 test_that("Histogram vs. Theoretical PDF plot matches", {
   plotName <- results[["results"]][["mleContainer"]][["collection"]][["mleContainer_mleFitAssessment"]][["collection"]][["mleContainer_mleFitAssessment_estPDF"]][["data"]]
   testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-  jaspTools::expect_equal_plots(testPlot, "histogram-vs-theoretical-pdf", dir="LDchisq")
+  expect_equal_plots(testPlot, "histogram-vs-theoretical-pdf", dir="LDchisq")
 })
 
 test_that("Fit Statistics table results match", {
   table <- results[["results"]][["mleContainer"]][["collection"]][["mleContainer_mleFitAssessment"]][["collection"]][["mleContainer_mleFitAssessment_fitStatisticsTable"]][["data"]]
-  jaspTools::expect_equal_tables(table,
-                      list(0.987805623863763, 0.0141898736833013, "Kolmogorov-Smirnov", 0.993943245701122,
-                           0.0225598781379213, "Cram<unicode>r-von Mises", 0.999462835577231,
-                           0.133476420881834, "Anderson-Darling"))
+  expect_equal_tables(table,
+                      list(0.98827693435693, 0.0141404497973602, "Kolmogorov-Smirnov", 0.994063879727108,
+                           0.0224779929435535, "Cram<unicode>r-von Mises", 0.999468456836373,
+                           0.133316493982989, "Anderson-Darling"))
 })
 
 test_that("P-P plot matches", {
   plotName <- results[["results"]][["mleContainer"]][["collection"]][["mleContainer_mleFitAssessment"]][["collection"]][["mleContainer_mleFitAssessment_ppplot"]][["data"]]
   testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-  jaspTools::expect_equal_plots(testPlot, "p-p-plot", dir="LDchisq")
+  expect_equal_plots(testPlot, "p-p-plot", dir="LDchisq")
 })
 
 test_that("Q-Q plot matches", {
   plotName <- results[["results"]][["mleContainer"]][["collection"]][["mleContainer_mleFitAssessment"]][["collection"]][["mleContainer_mleFitAssessment_qqplot"]][["data"]]
   testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-  jaspTools::expect_equal_plots(testPlot, "q-q-plot", dir="LDchisq")
+  expect_equal_plots(testPlot, "q-q-plot", dir="LDchisq")
 })
 
 test_that("Cumulative Probability Plot matches", {
   plotName <- results[["results"]][["plotCDF"]][["collection"]][["plotCDF_cdfPlot"]][["data"]]
   testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-  jaspTools::expect_equal_plots(testPlot, "cumulative-probability-plot", dir="LDchisq")
+  expect_equal_plots(testPlot, "cumulative-probability-plot", dir="LDchisq")
 })
 
 test_that("Density Plot matches", {
   plotName <- results[["results"]][["plotPDF"]][["collection"]][["plotPDF_pdfPlot"]][["data"]]
   testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-  jaspTools::expect_equal_plots(testPlot, "density-plot", dir="LDchisq")
+  expect_equal_plots(testPlot, "density-plot", dir="LDchisq")
 })
 
 test_that("Quantile Plot matches", {
   plotName <- results[["results"]][["plotQF"]][["collection"]][["plotQF_qfPlot"]][["data"]]
   testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
-  jaspTools::expect_equal_plots(testPlot, "quantile-plot", dir="LDchisq")
+  expect_equal_plots(testPlot, "quantile-plot", dir="LDchisq")
 })
