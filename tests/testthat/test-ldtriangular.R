7419af0e6fb4aad266ddd17eb8fd75b5b57a5e33
Kucharssim <kucharssim@gmail.com>
2021-01-12 13:37:56 +0100

    add triangular test

---
+++ tests/testthat/test-ldtriangular.R
@@ -0,0 +1,95 @@
+context("Discover Distributions - Triangular")
+
+options <- jaspTools::analysisOptions("LDtriangular")
+options$andersonDarling <- TRUE
+options$ciInterval <- TRUE
+options$ciIntervalInterval <- 0.95
+options$cramerVonMisses <- TRUE
+options$estCDF <- TRUE
+options$estPDF <- TRUE
+options$highlightDensity <- TRUE
+options$highlightProbability <- TRUE
+options$histogram <- FALSE
+options$kolmogorovSmirnov <- TRUE
+options$methodMLE <- TRUE
+options$newVariableName <- ""
+options$outputEstimates <- TRUE
+options$outputSE <- TRUE
+options$parsSupportMoments <- TRUE
+options$plotCDF <- TRUE
+options$plotQF <- TRUE
+options$ppplot <- TRUE
+options$qqplot <- TRUE
+options$variable <- "triangular"
+set.seed(1)
+
+results <- jaspTools::runAnalysis("LDtriangular",
+                                  data.frame(triangular = jaspDistributions:::rtriangular(n = 100, -1, 0, 0)),
+                                  options)
+
+test_that("Descriptives table results match", {
+  table <- results[["results"]][["dataContainer"]][["collection"]][["dataContainer_summary"]][["data"]]
+  jaspTools::expect_equal_tables(table,
+                                 list(0.872773903403654, 0.0231008819189913, -0.0122820673389992, -0.836354888401422,
+                                      -0.196171879565242, 0.317646083339521, 100, 0.3620049973184,
+                                      0.131047618083495, "triangular"))
+})
+
+test_that("Estimated Parameters table results match", {
+  table <- results[["results"]][["mleContainer"]][["collection"]][["mleContainer_estParametersTable"]][["data"]]
+  jaspTools::expect_equal_tables(table,
+                                 list(-0.891317307885553, -0.969576216603351, "a", 0.0399287483520583,
+                                      -0.813058399167756, 0.926436273265075, 0.833855517805458, "b",
+                                      0.0472359472877471, 1.01901702872469, -0.022636934371412, -0.0584371803743488,
+                                      "c", 0.0182657672719114, 0.0131633116315248))
+})
+
+test_that("Empirical vs. Theoretical CDF plot matches", {
+  plotName <- results[["results"]][["mleContainer"]][["collection"]][["mleContainer_mleFitAssessment"]][["collection"]][["mleContainer_mleFitAssessment_estCDF"]][["data"]]
+  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
+  jaspTools::expect_equal_plots(testPlot, "empirical-vs-theoretical-cdf", dir="LDtriangular")
+})
+
+test_that("Histogram vs. Theoretical PDF plot matches", {
+  plotName <- results[["results"]][["mleContainer"]][["collection"]][["mleContainer_mleFitAssessment"]][["collection"]][["mleContainer_mleFitAssessment_estPDF"]][["data"]]
+  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
+  jaspTools::expect_equal_plots(testPlot, "histogram-vs-theoretical-pdf", dir="LDtriangular")
+})
+
+test_that("Fit Statistics table results match", {
+  table <- results[["results"]][["mleContainer"]][["collection"]][["mleContainer_mleFitAssessment"]][["collection"]][["mleContainer_mleFitAssessment_fitStatisticsTable"]][["data"]]
+  jaspTools::expect_equal_tables(table,
+                                 list(0.802721108000785, 0.0643030863580119, "Kolmogorov-Smirnov", 0.73042073241695,
+                                      0.0736585513141384, "Cram<unicode>r-von Mises", 0.821622994509396,
+                                      0.426618952262345, "Anderson-Darling"))
+})
+
+test_that("P-P plot matches", {
+  plotName <- results[["results"]][["mleContainer"]][["collection"]][["mleContainer_mleFitAssessment"]][["collection"]][["mleContainer_mleFitAssessment_ppplot"]][["data"]]
+  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
+  jaspTools::expect_equal_plots(testPlot, "p-p-plot", dir="LDtriangular")
+})
+
+test_that("Q-Q plot matches", {
+  plotName <- results[["results"]][["mleContainer"]][["collection"]][["mleContainer_mleFitAssessment"]][["collection"]][["mleContainer_mleFitAssessment_qqplot"]][["data"]]
+  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
+  jaspTools::expect_equal_plots(testPlot, "q-q-plot", dir="LDtriangular")
+})
+
+test_that("Cumulative Probability Plot matches", {
+  plotName <- results[["results"]][["plotCDF"]][["collection"]][["plotCDF_cdfPlot"]][["data"]]
+  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
+  jaspTools::expect_equal_plots(testPlot, "cumulative-probability-plot", dir="LDtriangular")
+})
+
+test_that("Density Plot matches", {
+  plotName <- results[["results"]][["plotPDF"]][["collection"]][["plotPDF_pdfPlot"]][["data"]]
+  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
+  jaspTools::expect_equal_plots(testPlot, "density-plot", dir="LDtriangular")
+})
+
+test_that("Quantile Plot matches", {
+  plotName <- results[["results"]][["plotQF"]][["collection"]][["plotQF_qfPlot"]][["data"]]
+  testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
+  jaspTools::expect_equal_plots(testPlot, "quantile-plot", dir="LDtriangular")
+})
